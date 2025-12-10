021e06b00cbeeb292eab1c16c53c7204821bd2aa
AstrexAI <astrexai@gmail.com>
2025-12-10 21:06:16 +0200

    Add root route

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import crypto from "crypto";
 
@@ -31,43 +30,8 @@ app.post("/activate", (req, res) => {
     res.json({ success: true });
 });
 
-app.get('/', (req, res) => {
-  res.send("License server is working!");
+app.get("/", (req, res) => {
+  res.send("EuroBG License Server is running.");
 });
 
 app.listen(3000, () => console.log("License server running on :3000"));
-=======
-import express from "express";
-import crypto from "crypto";
-
-const app = express();
-app.use(express.json());
-
-let licenseDB = {}; // временно – после ще сложим база
-
-app.post("/generate", (req, res) => {
-    const { count } = req.body;
-    let keys = [];
-
-    for (let i = 0; i < count; i++) {
-        const key = crypto.randomBytes(16).toString("hex");
-        licenseDB[key] = { active: false };
-        keys.push(key);
-    }
-
-    res.json({ keys });
-});
-
-app.post("/activate", (req, res) => {
-    const { key } = req.body;
-
-    if (!licenseDB[key]) {
-        return res.status(400).json({ error: "Invalid key" });
-    }
-
-    licenseDB[key].active = true;
-    res.json({ success: true });
-});
-
-app.listen(3000, () => console.log("License server running on :3000"));
->>>>>>> 1d8f86d99342f39564651b7b993c8ee6182fef64
